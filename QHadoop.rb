2c1a82d7feb406aa1932fa5c2e93e3e0dbaeefa9
IsaiahGardeenVersata <51798395+IsaiahGardeenVersata@users.noreply.github.com>
2019-06-17 14:46:19 -0500

    Update and rename Hadoop.rb to QHadoop.rb

--- QHadoop.rb
+++ QHadoop.rb
@@ -1,4 +1,4 @@
-class Hadoop < Formula
+class QHadoop < Formula
   desc "Framework for distributed processing of large data sets"
   homepage "https://hadoop.apache.org/"
   url "https://www.apache.org/dyn/closer.cgi?path=hadoop/common/hadoop-2.8.5/hadoop-2.8.5.tar.gz"
@@ -21,6 +21,6 @@ class Hadoop < Formula
   end
 
   test do
-    system bin/"hadoop", "fs", "-ls"
+    system bin/"qhadoop", "fs", "-ls"
   end
 end
