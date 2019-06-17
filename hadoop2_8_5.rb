cdbd4e58a2df52ddf8bf3032c549752d782862ea
IsaiahGardeenVersata <51798395+IsaiahGardeenVersata@users.noreply.github.com>
2019-06-17 14:12:02 -0500

    Update hadoop2_8_5.rb

--- hadoop2_8_5.rb
+++ hadoop2_8_5.rb
@@ -1,4 +1,4 @@
-class Hadoop < Formula
+class hadoop2_8_5 < Formula
   desc "Framework for distributed processing of large data sets"
   homepage "https://hadoop.apache.org/"
   url "https://www.apache.org/dyn/closer.cgi?path=hadoop/common/hadoop-2.8.5/hadoop-2.8.5.tar.gz"
